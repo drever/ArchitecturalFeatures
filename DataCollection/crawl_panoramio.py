d3d34eb6dba4d007290ec1ddf63810aaa9fa6db2
Johannes Drever <johannes.drever@linova.de>
2013-01-27 16:51:44 +0100

    Added saving of image info.

--- DataCollection/crawl_panoramio.py
+++ DataCollection/crawl_panoramio.py
@@ -3,13 +3,17 @@ import json
 
 import urllib
 
+import os
+import pickle
+
 data_directory = "../Data/"
+image_server_url = "www.panoramio.com"
 
 def main():
-    panoramio_response = query_panoramio()
-    query_images(panoramio_response)
+    image_list = query_image_list()
+    query_images(image_list, "test")
 
-def query_panoramio(tag=None, size='medium', setp='public', rangep=(0, 1), region=((-180, -90), (180, 90)), mapFilter=False):
+def query_image_list(tag=None, size='medium', setp='public', rangep=(0, 100), region=((-180, -90), (180, 90)), mapFilter=False):
     query = "/map/get_panoramas.php?set=" + setp
     query += "&from=" + str(rangep[0]) + "&to=" + str(rangep[1])
     query += "&minx=" + str(region[0][0]) + "&miny=" + str(region[0][1]) + "&maxx" + str(region[1][0]) + "&maxy=" + str(region[1][1])
@@ -19,20 +23,23 @@ def query_panoramio(tag=None, size='medium', setp='public', rangep=(0, 1), regio
         query += "&tag=" + tag
 
     print "Sending query " + query
-    connection = httplib.HTTPConnection("www.panoramio.com")
+    connection = httplib.HTTPConnection(image_server_url)
     connection.request("GET", query)
     response = connection.getresponse()
     data = response.read()
     jdata = json.loads(data)
+    print "Recieved list with " + str(len(jdata["photos"])) + " image(s)."
     return jdata
 
-def query_images(panoramio_response):
-    for photo in panoramio_response["photos"]:
-        print "loading from" + photo["photo_url"]
-
-        file_name = data_directory + photo["photo_file_url"].split('/')[-1]
+def query_images(image_list, list_name):
+    ldd = data_directory + list_name + "/"
+    if not os.path.exists(ldd):
+        os.makedirs(ldd)
+    pickle.dump(image_list, open(ldd + "metadata.p", "wb"))
+    for photo in image_list["photos"]:
+        print "loading from " + photo["photo_url"]
+        file_name = ldd + photo["photo_file_url"].split('/')[-1]
         urllib.urlretrieve(photo["photo_file_url"], file_name)
-
         print "saving to " + file_name
 
 if __name__ == "__main__":
